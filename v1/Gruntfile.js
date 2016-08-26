a403bd8ea08064788d1ef1046acce736e62fafd7
Rick Pasetto <rpasetto@tableau.com>
2016-08-26 14:30:57 -0700

    copy the v1 implementation aside (just in case)

---
+++ v1/Gruntfile.js
@@ -0,0 +1,85 @@
+// Generated on 2016-03-04 using generator-web-data-connector 1.0.1
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: [
+        'Gruntfile.js',
+        'js/*.js',
+        '!js/scripts.min.js'
+      ]
+    },
+    concat: {
+      js: {
+        options: {
+          separator: ';'
+        },
+        src: [
+          'bower_components/jquery/dist/jquery.js',
+          'bower_components/tableau/dist/*.js',
+          'bower_components/bootstrap/dist/js/bootstrap.js',
+          'src/wrapper.js',
+          'src/**/*.js'
+        ],
+        dest: 'build/all.js'
+      }
+    },
+    uglify: {
+      options: {
+        compress: true,
+        mangle: true,
+        sourceMap: true
+      },
+      target: {
+        src: 'build/all.js',
+        dest: 'build/all.min.js'
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          base: './',
+          port: 9001
+        }
+      }
+    },
+    watch: {
+      scripts: {
+        files: 'src/**/*.js',
+        tasks: [
+          'jshint',
+          'concat',
+          'uglify'
+        ]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', [
+    'build',
+    'run'
+  ]);
+
+  grunt.registerTask('run', [
+    'connect:server',
+    'watch'
+  ]);
+
+  grunt.registerTask('build', [
+    'jshint',
+    'concat',
+    'uglify'
+  ]);
+};
